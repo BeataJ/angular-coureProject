2ccc16afbdf9436a26d6ecfaf3a277d7ba3e9238
beataj <beata@emc2wave.com>
2021-06-22 14:50:20 -0700

    add app-recipes app-shoping-list do app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShopingListComponent } from './shoping-list/shoping-list.component';
+import { ShopingEditComponent } from './shoping-list/shoping-edit/shoping-edit.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { ShopingListComponent } from './shoping-list/shoping-list.component';
     RecipeListComponent,
     RecipeDetailComponent,
     RecipeItemComponent,
-    ShopingListComponent
+    ShopingListComponent,
+    ShopingEditComponent
   ],
   imports: [
     BrowserModule
