92e3abddd952a841d3295095c6ce1d32d03ac497
beataj <beata@emc2wave.com>
2021-06-30 16:06:10 -0700

    emit click event

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   collapsed = true;
+  @Output() futureSlected = new EventEmitter<string>()
 
   constructor() { }
 
@@ -14,7 +15,7 @@ export class HeaderComponent implements OnInit {
   }
 
   onSelect(x: string) {
-
+    this.futureSlected.emit(x);
   }
 
 }
