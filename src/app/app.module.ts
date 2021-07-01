f42c75dc7c6922ce6e6df682e2df49a08878aa8c
beataj <beata@emc2wave.com>
2021-06-30 19:23:32 -0700

    import FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -22,7 +23,8 @@ import { ShopingEditComponent } from './shoping-list/shoping-edit/shoping-edit.c
     ShopingEditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
