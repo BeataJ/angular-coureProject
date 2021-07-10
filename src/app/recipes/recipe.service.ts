f99f8e499c5d6d5fa485b933edeecd819db91d37
beataj <beata@emc2wave.com>
2021-07-10 15:18:16 -0700

    add recipies [] to recipe service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -4,6 +4,10 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class RecipeService {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe 1', 'This is simply a test', 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fkeeprecipes.com%2Fsites%2Fkeeprecipes%2Ffiles%2Facar.jpg&f=1&nofb=1'),
+    new Recipe('A Test Recipe 2', 'This is simply a test', 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.hy-vee.com%2Fwebres%2FImage%2FRecipes%2FVEGGIE-CHEESE-WRAPS.jpg&f=1&nofb=1')
+  ];
 
   constructor() { }
 }
