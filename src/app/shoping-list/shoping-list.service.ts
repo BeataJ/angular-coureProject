b42ae3c1efc30440fee8dcf16ecc923efcce35b9
beataj <beata@emc2wave.com>
2021-07-12 16:44:59 -0700

    add data array to shopinglist service

--- src/app/shoping-list/shoping-list.service.ts
+++ src/app/shoping-list/shoping-list.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShopingListService {
+  private ingredients:Ingredient[] =[
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10)
+  ];
 
-  constructor() { }
+  // constructor() { }
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+
 }
