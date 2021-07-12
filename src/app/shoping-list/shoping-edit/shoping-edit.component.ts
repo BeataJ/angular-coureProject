3310e7c5b2d7006e032eaea012145c2663473e89
beataj <beata@emc2wave.com>
2021-07-12 16:55:53 -0700

    remove add ingredients

--- src/app/shoping-list/shoping-edit/shoping-edit.component.ts
+++ src/app/shoping-list/shoping-edit/shoping-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Component, ElementRef,  OnInit, ViewChild } from '@angular/core';
 
 import { Ingredient } from 'src/app/shared/ingredient.model';
 
@@ -11,7 +11,7 @@ import { Ingredient } from 'src/app/shared/ingredient.model';
 export class ShopingEditComponent implements OnInit {
   @ViewChild('nameInput', {static: false }) nameInputRef: ElementRef;
   @ViewChild('amountInput', {static: false }) amountInputRef: ElementRef;
-  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+  
 
   constructor() { }
 
@@ -23,7 +23,7 @@ export class ShopingEditComponent implements OnInit {
     const ingAmount = this.amountInputRef.nativeElement.value;
     const newIngredient = new Ingredient(ingName,ingAmount);
 
-    this.ingredientAdded.emit(newIngredient);
+    
   }
 
 }
