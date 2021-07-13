80cd49cedd5d2d80d2a8117729835774f28ece94
beataj <beata@emc2wave.com>
2021-07-12 17:16:17 -0700

    add ingredientsChanges emiter

--- src/app/shoping-list/shoping-list.service.ts
+++ src/app/shoping-list/shoping-list.service.ts
@@ -1,10 +1,11 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShopingListService {
+  ingredientsChanges = new EventEmitter<Ingredient[]>()
   private ingredients:Ingredient[] =[
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10)
@@ -18,6 +19,7 @@ export class ShopingListService {
 
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
+    this.ingredientsChanges.emit(this.ingredients.slice());
   }
 
 }
