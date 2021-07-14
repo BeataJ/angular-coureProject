c30bee9f3a16f48609c57e0bbea8056323afd665
beataj <beata@emc2wave.com>
2021-07-13 17:08:09 -0700

    add ingredients

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
 
 @Injectable({
@@ -8,13 +9,35 @@ export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>()
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe 1', 'This is simply a test', 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fkeeprecipes.com%2Fsites%2Fkeeprecipes%2Ffiles%2Facar.jpg&f=1&nofb=1'),
-    new Recipe('A Test Recipe 2', 'This is simply a test', 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.hy-vee.com%2Fwebres%2FImage%2FRecipes%2FVEGGIE-CHEESE-WRAPS.jpg&f=1&nofb=1')
+    new Recipe(
+      'Chinese cucina',
+      'Super sweet and tasty cucinia.',
+      'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fkeeprecipes.com%2Fsites%2Fkeeprecipes%2Ffiles%2Facar.jpg&f=1&nofb=1',
+      [
+        new Ingredient('cucina', 2),
+        new Ingredient('pepper', 1),
+        new Ingredient('sweet sos', 1),
+        new Ingredient('pinch of sesam',0)
+      ]
+      ),
+    new Recipe(
+      '"rollo" sandwich',
+      'good for work or school',
+      'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.hy-vee.com%2Fwebres%2FImage%2FRecipes%2FVEGGIE-CHEESE-WRAPS.jpg&f=1&nofb=1',
+      [
+        new Ingredient('roll', 1),
+        new Ingredient('slice of cheese', 2),
+        new Ingredient('slice of cucumber', 3),
+        new Ingredient('pieces of onion', 6 ),
+        new Ingredient('pieces of carrots', 12),
+      ]
+      )
   ];
 
   getRecipes() {
     return this.recipes.slice();
   }
+
 
 
   // constructor() { }
