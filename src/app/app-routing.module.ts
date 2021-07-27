acfd963d9719e022259058b2b806570b42535e78
beataj <beata@emc2wave.com>
2021-07-27 14:00:12 -0700

    add exports [] to app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,8 @@ const appRoutes: Routes = [
 ]
 
 @NgModule({
-    imports: [RouterModule.forRoot(appRoutes)]
+    imports: [RouterModule.forRoot(appRoutes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule {
 
