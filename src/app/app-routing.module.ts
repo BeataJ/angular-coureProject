7d3ab5b637cc98591a12019526c3b732b4ae3167
beataj <beata@emc2wave.com>
2021-07-27 13:57:04 -0700

    import RouterModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from "@angular/core";
-import { Router } from "@angular/router";
+import { Routes, RouterModule } from "@angular/router";
 
 import { RecipeService } from "./recipes/recipe.service";
 import { ShopingListService } from "./shoping-list/shoping-list.service";
 
-const appRouters: Router = [
+const appRoutes: Routes = [
     { path: '', redirectTo: '/recipes' },
     { path: 'recipes', component: RecipeService},
     { path: 'shoping-list', component: ShopingListService }
 ]
 
 @NgModule({
-
+    imports: [RouterModule.forRoot(appRoutes)]
 })
 export class AppRoutingModule {
 
