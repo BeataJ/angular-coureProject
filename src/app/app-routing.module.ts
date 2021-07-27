a466fc35b5b968155962b4877c9a3f0151e72686
beataj <beata@emc2wave.com>
2021-07-27 13:51:36 -0700

    add appRouters with 2 route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from "@angular/core";
+import { Router } from "@angular/router";
+
+import { RecipeService } from "./recipes/recipe.service";
+import { ShopingListService } from "./shoping-list/shoping-list.service";
+
+const appRouters: Router = [
+    { path: 'recipes', component: RecipeService},
+    { path: 'shoping-list', component: ShopingListService }
+]
 
 @NgModule({
-    
+
 })
 export class AppRoutingModule {
 
