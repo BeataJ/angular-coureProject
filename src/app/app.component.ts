9b7d15446388aef7eadd1e9b69ce5727c85a32d3
beataj <beata@emc2wave.com>
2021-08-02 14:29:16 -0700

    add Subject()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'courseProject';
-  loadedFeature = 'recipe'
-
-  onNavigate(y: string) {
-    this.loadedFeature = y
-  }
 }
