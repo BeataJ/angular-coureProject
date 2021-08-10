9906ccb7cae1aa4066815c37f7d3353f45d8a316
beataj <beata@emc2wave.com>
2021-08-10 15:39:21 -0700

    add validators required

--- src/app/shoping-list/shoping-edit/shoping-edit.component.ts
+++ src/app/shoping-list/shoping-edit/shoping-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef,  OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 import { Ingredient } from 'src/app/shared/ingredient.model';
