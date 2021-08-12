fb74581a4467a0e313d472d4901a894e0834de8c
beataj <beata@emc2wave.com>
2021-08-12 14:36:21 -0700

    subscribe startedEdit

--- src/app/shoping-list/shoping-edit/shoping-edit.component.ts
+++ src/app/shoping-list/shoping-edit/shoping-edit.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShopingListService } from '../shoping-list.service';
@@ -11,12 +12,14 @@ import { ShopingListService } from '../shoping-list.service';
   styleUrls: ['./shoping-edit.component.css']
 })
 export class ShopingEditComponent implements OnInit {
-  
+  subscription: Subscription
   
 
   constructor(private slService: ShopingListService) { }
 
   ngOnInit(): void {
+    this.subscription = this.slService.startedEditing
+      .subscribe();
   }
 
   onAddItem(form: NgForm) {
