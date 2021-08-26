ac90a12a833388b95f2d8b968be899c85cd8622f
beataj <beata@emc2wave.com>
2021-08-25 17:28:52 -0700

    create empty addRecipe() updateRecipe()

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 
 @Component({
@@ -30,7 +31,7 @@ export class RecipeEditComponent implements OnInit {
   }
 
   onSubmit() {
-    console.log(this.recipeForm)
+    
   }
 
   onAddIngredient() {
@@ -82,5 +83,9 @@ export class RecipeEditComponent implements OnInit {
       'ingredients': recipeIngredients
     });
   }
+
+  addRecipe(recipe: Recipe) {}
+
+  updateRecipe(index: number, newRecipe: Recipe) {}
 
 }
