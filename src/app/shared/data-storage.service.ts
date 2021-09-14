d8f2a78bd9f27d4d3039f37ab47f6fe1ba3a0d38
beataj <beata@emc2wave.com>
2021-09-13 23:05:12 -0700

    add recipeService storeRecipes()

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 
+import { RecipeService } from "../recipes/recipe.service";
+import { environment } from "../../../src/environment-app"
+
 @Injectable({
     providedIn: 'root'
 })
 export class DataStorageService {
-    constructor(private http: HttpClient) {}
+    constructor(
+        private http: HttpClient,
+        private recipeService: RecipeService
+        ) {}
+
+    storeRecipes() {
+       const recipes = this.recipeService.getRecipes(); 
+       return this.http.put(environment.apiUrl, recipes) 
+    }
 }
