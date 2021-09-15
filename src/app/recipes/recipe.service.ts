4e71d01e453a6264eb84ed2340bf263d714af167
beataj <beata@emc2wave.com>
2021-09-15 16:03:30 -0700

    add private recipes: Recipe[] = [];

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -11,31 +11,33 @@ import { Recipe } from './recipe.model';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>()
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Chinese cucina',
-      'Super sweet and tasty cucinia.',
-      'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fkeeprecipes.com%2Fsites%2Fkeeprecipes%2Ffiles%2Facar.jpg&f=1&nofb=1',
-      [
-        new Ingredient('cucina', 2),
-        new Ingredient('pepper', 1),
-        new Ingredient('sweet sos', 1),
-        new Ingredient('pinch of sesam',3)
-      ]
-      ),
-    new Recipe(
-      '"rollo" sandwich',
-      'good for work or school',
-      'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.hy-vee.com%2Fwebres%2FImage%2FRecipes%2FVEGGIE-CHEESE-WRAPS.jpg&f=1&nofb=1',
-      [
-        new Ingredient('roll', 1),
-        new Ingredient('slice of cheese', 2),
-        new Ingredient('slice of cucumber', 3),
-        new Ingredient('pieces of onion', 6 ),
-        new Ingredient('pieces of carrots', 12),
-      ]
-      )
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Chinese cucina',
+  //     'Super sweet and tasty cucinia.',
+  //     'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fkeeprecipes.com%2Fsites%2Fkeeprecipes%2Ffiles%2Facar.jpg&f=1&nofb=1',
+  //     [
+  //       new Ingredient('cucina', 2),
+  //       new Ingredient('pepper', 1),
+  //       new Ingredient('sweet sos', 1),
+  //       new Ingredient('pinch of sesam',3)
+  //     ]
+  //     ),
+  //   new Recipe(
+  //     '"rollo" sandwich',
+  //     'good for work or school',
+  //     'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.hy-vee.com%2Fwebres%2FImage%2FRecipes%2FVEGGIE-CHEESE-WRAPS.jpg&f=1&nofb=1',
+  //     [
+  //       new Ingredient('roll', 1),
+  //       new Ingredient('slice of cheese', 2),
+  //       new Ingredient('slice of cucumber', 3),
+  //       new Ingredient('pieces of onion', 6 ),
+  //       new Ingredient('pieces of carrots', 12),
+  //     ]
+  //     )
+  // ];
+
+  private recipes: Recipe[] = [];
 
   constructor(private slService: ShopingListService) { }
 
