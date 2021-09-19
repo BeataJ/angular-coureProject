c8f406175b1ebc0901a82386ab25ba08c3dd27ec
beataj <beata@emc2wave.com>
2021-09-18 19:56:48 -0700

    add post endpoint

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 
+import { environment } from "../../environment-app";
+
 @Injectable({
     providedIn: 'root'
 })
@@ -8,5 +10,12 @@ export class AuthService {
 
     constructor(private http: HttpClient) {}
 
-    signup() {}
+    signup(email: string, password: string) {
+       this.http.post(environment.authUrl,
+        {
+            email,
+            password,
+            returnSecureToken: true
+        }) 
+    }
 }
