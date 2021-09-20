8d5f52c9961c14457dc3a69b4cb9bf24de699739
beataj <beata@emc2wave.com>
2021-09-19 18:52:19 -0700

    add spinner html

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,3 @@
-import { ThisReceiver } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthService } from './auth.service';
