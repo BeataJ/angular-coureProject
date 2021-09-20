be0f948bdd5fb3a003e7bd5d52f626707be136aa
beataj <beata@emc2wave.com>
2021-09-19 20:10:34 -0700

    subscribe errorMessage

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -37,9 +37,9 @@ export class AuthComponent implements OnInit {
           console.log(resData);
           this.isLoading = false;
         },
-          errorRes => {
-            console.log(errorRes);
-            
+          errorMessage => {
+            console.log(errorMessage);
+            this.error = errorMessage;
             this.isLoading = false;
           }
         );
