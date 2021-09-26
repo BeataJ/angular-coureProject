fffc3356beba731dd1a7090525064a78ff0e6d70
beataj <beata@emc2wave.com>
2021-09-26 16:45:34 -0700

    add logout method

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -63,6 +63,11 @@ export class AuthService {
         )
     }
 
+    logout() {
+        this.user.next(null)
+    }
+
+
     private handleAuthentication(email: string,userId: string, token: string, expiresIn: number) {
         const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
         const user = new User(email, userId, token, expirationDate);
