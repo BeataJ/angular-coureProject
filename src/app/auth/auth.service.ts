bedcae4ee137bd2bd9663aa524936b7e4bfd1f04
beataj <beata@emc2wave.com>
2021-09-26 17:02:24 -0700

    add localStorage

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -77,7 +77,8 @@ export class AuthService {
     private handleAuthentication(email: string,userId: string, token: string, expiresIn: number) {
         const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
         const user = new User(email, userId, token, expirationDate);
-        this.user.next(user)
+        this.user.next(user);
+        localStorage.setItem('userData', JSON.stringify(user));
     }
 
     private handleError(errorRes: HttpErrorResponse) {
