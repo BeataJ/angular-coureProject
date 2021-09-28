5e5030e777e241687c73ce321415f01fa96f77e9
beataj <beata@emc2wave.com>
2021-09-28 15:12:27 -0700

    add take operator

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
 import { Observable } from "rxjs";
-import { map } from "rxjs/operators"
+import { map, take } from "rxjs/operators"
 
 import { AuthService } from "./auth.service";
 
@@ -15,7 +15,9 @@ export class AuthGuard implements CanActivate {
         ){}
 
     canActivate(route: ActivatedRouteSnapshot, router: RouterStateSnapshot): boolean | UrlTree |  Promise<boolean | UrlTree> |  Observable<boolean | UrlTree>{
-        return this.authservice.user.pipe(map(user => {
+        return this.authservice.user.pipe(
+            take(1),
+            map(user => {
             const isAuth = !!user;
             if(isAuth) {
                 return true;
