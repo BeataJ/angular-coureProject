1ac651eb85811c0bef5c3911c0de445f5ae62a51
beataj <beata@emc2wave.com>
2021-10-06 16:08:29 -0700

    create recipes-routing.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,30 +1,13 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
-import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
-
-import { RecipesComponent } from "./recipes/recipes.component";
-import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
 import { ShopingListComponent } from "./shoping-list/shoping-list.component";
-import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
-import { RecipesResolverService } from "./recipes/recipes-resolver.service";
 import { AuthComponent } from "./auth/auth.component";
-import { AuthGuard } from "./auth/auth.guard";
+
 
 
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-    { 
-        path: 'recipes', 
-        component: RecipesComponent,
-        canActivate: [AuthGuard], 
-        children: [
-            { path: '', component: RecipeStartComponent},
-            { path: 'new', component: RecipeEditComponent },
-            { path: ':id', component: RecipeDetailComponent, resolve: [RecipesResolverService] },
-            {  path: ':id/edit', component: RecipeEditComponent, resolve: [RecipesResolverService]}
-        ]
-    },
     { path: 'shoping-list', component: ShopingListComponent },
     { path: 'auth', component: AuthComponent}
 ]
