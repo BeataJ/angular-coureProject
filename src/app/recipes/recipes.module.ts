1a569ef90083d2a65b33b18e71a7786d5f2018b3
beataj <beata@emc2wave.com>
2021-10-05 17:55:31 -0700

    imports RouterModule,         CommonModule,         ReactiveFormsModule

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -1,4 +1,7 @@
 import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { CommonModule } from "@angular/common";
+import { ReactiveFormsModule } from "@angular/forms";
 
 import { RecipesComponent } from "./recipes.component";
 import { RecipeListComponent } from "./recipe-list/recipe-list.component";
@@ -6,6 +9,9 @@ import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
 import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
 import { RecipeItemComponent } from "./recipe-list/recipe-item/recipe-item.component";
 import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
+
+
+
 
 @NgModule({
     declarations: [
@@ -16,6 +22,11 @@ import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
         RecipeStartComponent,
         RecipeEditComponent,
     ],
+    imports: [
+        RouterModule,
+        CommonModule,
+        ReactiveFormsModule
+     ],
     exports: [
         RecipesComponent,
         RecipeListComponent,
