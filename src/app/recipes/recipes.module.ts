a4ec2751facbeab5d18318a61352ffca6d168f42
beataj <beata@emc2wave.com>
2021-10-06 22:32:10 -0700

    delete exports from recipies module

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -28,15 +28,7 @@ import { RecipesRoutingModule } from "./recipes-routing.module";
         CommonModule,
         ReactiveFormsModule,
         RecipesRoutingModule
-     ],
-    exports: [
-        RecipesComponent,
-        RecipeListComponent,
-        RecipeDetailComponent,
-        RecipeItemComponent,
-        RecipeStartComponent,
-        RecipeEditComponent,
-    ]
+     ]
 })
 export class RecipesModule {
 }
