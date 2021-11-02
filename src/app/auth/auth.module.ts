b92ca85171405e554a531a00a3dd5a655475457e
beataj <beata@emc2wave.com>
2021-11-01 17:20:06 -0700

    add RouterModule.forChild()

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
 
 import { AuthComponent } from "./auth.component";
 
@@ -10,7 +11,8 @@ import { AuthComponent } from "./auth.component";
     ],
     imports: [
         CommonModule,
-        FormsModule
+        FormsModule,
+        RouterModule.forChild([{ path: 'auth', component: AuthComponent }] )
     ]
 })
 export class AuthModule {
