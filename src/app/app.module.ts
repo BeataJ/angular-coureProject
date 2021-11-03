4c8c7d1eb078459b102f2f455bfea89da95ce48c
beataj <beata@emc2wave.com>
2021-11-02 21:03:52 -0700

    correct shoppingList

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
-import { AuthModule } from './auth/auth.module';
+
 
 @NgModule({
   declarations: [
@@ -19,7 +19,6 @@ import { AuthModule } from './auth/auth.module';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    AuthModule,
     SharedModule,
     CoreModule
   ],
