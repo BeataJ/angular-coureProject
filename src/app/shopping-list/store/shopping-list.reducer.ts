2a27d831a5785e25706efce4b53b30d98293fa5f
beataj <beata@emc2wave.com>
2021-11-09 18:41:20 -0800

    add code start_edit stop_edit

--- src/app/shopping-list/store/shopping-list.reducer.ts
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -55,7 +55,19 @@ export function shoppingListReducer(
                 ingredients: state.ingredients.filter((ig, igIndex) => {
                     return igIndex !== action.payload;
                 })
-            };            
+            }; 
+        case ShoppingListActions.START_EDIT:
+            return {
+                ...state,
+                editedIngredientIndex: action.payload,
+                editedIngredient: { ...state.ingredients[action.payload] }
+            }
+        case ShoppingListActions.STOP_EDIT:
+            return {
+                ...state,
+                editedIngredient: null,
+                editedIngredientIndex: -1
+            }                   
         default:
             return state;
     };
