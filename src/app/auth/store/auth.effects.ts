7e481478dc3cd0bb1ce3816f0e4e6e742730362e
beataj <beata@emc2wave.com>
2021-11-15 17:55:09 -0800

    create auth.effects.ts

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -1,11 +1,35 @@
 import { Actions, ofType } from "@ngrx/effects";
+import { switchMap } from "rxjs/operators";
+import { HttpClient} from "@angular/common/http";
 
-import * as AuthActions from './auth.actions'
+import * as AuthActions from './auth.actions';
+import { environment } from '../../../environment-app'
+
+export interface AuthResponseData {
+    idToken: string,
+    email: string,
+    refreshToken: string,
+    expiresIn: string,
+    localId: string,
+    registered?: boolean
+}
 
 export class AuthEffects {
     authLogin = this.actions$.pipe(
-        ofType(AuthActions.LOGIN_START)
+        ofType(AuthActions.LOGIN_START),
+        switchMap((authData: AuthActions.LoginStart) => {
+            return this.http.post<AuthResponseData>(environment.authLogin,
+                {
+                    email: authData.payload.email,
+                    password: authData.payload.password,
+                    returnSecureToken: true
+                }
+            )
+        })
     )
 
-    constructor(private actions$: Actions) {}
+    constructor(
+        private actions$: Actions,
+        private http: HttpClient
+        ) {}
 }
