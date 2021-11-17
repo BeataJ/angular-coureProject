7ecbaf7e44d51799bcf2c33e96152f5f08c2816c
beataj <beata@emc2wave.com>
2021-11-16 21:08:33 -0800

    add export const LOGIN_FAIL = '[Auth] Login Fail';

--- src/app/auth/store/auth.actions.ts
+++ src/app/auth/store/auth.actions.ts
@@ -1,6 +1,8 @@
+import { Actions } from "@ngrx/effects";
 import { Action } from "@ngrx/store";
 
-export const LOGIN_START = '[Auth] Login Start'
+export const LOGIN_START = '[Auth] Login Start';
+export const LOGIN_FAIL = '[Auth] Login Fail';
 export const LOGIN = '[Auth] Login';
 export const LOGOUT = '[Auth] Logout';
 
@@ -25,6 +27,12 @@ export class LoginStart implements Action {
     readonly type = LOGIN_START;
 
     constructor(public payload: { email: string, password: string }) {}
+}
+
+export class LoginFail implements Action {
+    readonly type = LOGIN_FAIL;
+
+    constructor(public payload: string) {}
 }
 
 export type AuthActions = Login | Logout;
