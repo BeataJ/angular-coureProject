a742742fff81a90a601c7c4ecae3427fb1727255
beataj <beata@emc2wave.com>
2021-11-16 23:06:58 -0800

    add authSuccess effect

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -1,11 +1,13 @@
 import { Actions, createEffect, Effect, ofType } from "@ngrx/effects";
-import { catchError, switchMap, map } from "rxjs/operators";
+import { catchError, switchMap, map, tap } from "rxjs/operators";
 import { of } from "rxjs";
 import { HttpClient} from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
 
 import * as AuthActions from './auth.actions';
 import { environment } from '../../../environment-app'
+
 
 
 
@@ -20,36 +22,43 @@ export interface AuthResponseData {
 @Injectable()
 export class AuthEffects {
     @Effect()
+    authLogin = this.actions$.pipe(
+            ofType(AuthActions.LOGIN_START),
+            switchMap((authData: AuthActions.LoginStart) => {
+                return this.http.post<AuthResponseData>(environment.authLogin,
+                    {
+                        email: authData.payload.email,
+                        password: authData.payload.password,
+                        returnSecureToken: true
+                    }
+                ).pipe(
+                    map(resData => {
+                        const expirationDate = new Date(new Date().getTime() + +resData.expiresIn * 1000);
+                        return new AuthActions.Login({
+                            email: resData.email,
+                            userId: resData.localId,
+                            token: resData.idToken,
+                            expirationDate: expirationDate
+                        })
+                    }),
+                    catchError(error => {
+                        return of()
+                    })
+                );
+            })
+    );
 
-    authLogin =  this.actions$.pipe(
-        ofType(AuthActions.LOGIN_START),
-        switchMap((authData: AuthActions.LoginStart) => {
-            return this.http.post<AuthResponseData>(environment.authLogin,
-                {
-                    email: authData.payload.email,
-                    password: authData.payload.password,
-                    returnSecureToken: true
-                }
-            ).pipe(
-                map(resData => {
-                    const expirationDate = new Date(new Date().getTime() + +resData.expiresIn * 1000);
-                    return of(new AuthActions.Login({
-                        email: resData.email,
-                        userId: resData.localId,
-                        token: resData.idToken,
-                        expirationDate: expirationDate
-                    }))
-                }),
-                catchError(error => {
-                return of()
-            }))
-        })
-        
-    )
-     
+    @Effect({dispatch: false})
+    authSuccess = this.actions$.pipe(
+            ofType(AuthActions.LOGIN),
+            tap(() => {
+                this.router.navigate(['/']);
+            })
+        )
 
     constructor(
         private actions$: Actions,
-        private http: HttpClient
-        ) {}
+        private http: HttpClient,
+        private router: Router
+    ) {}
 }
