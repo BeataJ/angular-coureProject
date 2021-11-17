d35c42aedfd0a0388936b39ba7792fa399508347
beataj <beata@emc2wave.com>
2021-11-17 00:02:44 -0800

    add login fail effect

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -41,8 +41,27 @@ export class AuthEffects {
                             expirationDate: expirationDate
                         })
                     }),
-                    catchError(error => {
-                        return of()
+                    catchError(errorRes => {
+                        let errorMessage = 'An unknowerror occurred!'
+                        if (!errorRes.error || !errorRes.error.error) {
+                            return of(new AuthActions.LoginFail(errorMessage))
+                        };
+
+                        switch (errorRes.error.error.message) {
+                            case 'EMAIL_EXISTS':
+                                errorMessage = 'This email exists already';
+                                break;
+                            case 'EMAIL_NOT_FOUND':
+                                errorMessage = 'This email does not exist';
+                                break;
+                            case 'INVALID_PASSWORD':
+                                errorMessage = 'This password is not correct.';
+                                break;
+
+                        }
+                        return of(new AuthActions.LoginFail(errorMessage))
+
+                        return of(new AuthActions.LoginFail(errorMessage))
                     })
                 );
             })
