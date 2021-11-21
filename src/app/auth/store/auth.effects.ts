b7995f480e6008cc4211641bd522cd772eea0168
beataj <beata@emc2wave.com>
2021-11-20 19:58:22 -0800

    add store to auth comp

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -92,33 +92,15 @@ export class AuthEffects {
                     }
                 ).pipe(
                     map(resData => {
-                        const expirationDate = new Date(new Date().getTime() + +resData.expiresIn * 1000);
-                        return new AuthActions.AuthenticateSuccess({
-                            email: resData.email,
-                            userId: resData.localId,
-                            token: resData.idToken,
-                            expirationDate: expirationDate
-                        })
+                        return handleAuthentication(
+                            +resData.expiresIn,
+                            resData.email,
+                            resData.idToken,
+                            resData.localId
+                        )
                     }),
                     catchError(errorRes => {
-                        let errorMessage = 'An unknowerror occurred!'
-                        if (!errorRes.error || !errorRes.error.error) {
-                            return of(new AuthActions.AuthenticateFail(errorMessage))
-                        };
-
-                        switch (errorRes.error.error.message) {
-                            case 'EMAIL_EXISTS':
-                                errorMessage = 'This email exists already';
-                                break;
-                            case 'EMAIL_NOT_FOUND':
-                                errorMessage = 'This email does not exist';
-                                break;
-                            case 'INVALID_PASSWORD':
-                                errorMessage = 'This password is not correct.';
-                                break;
-
-                        }
-                        return of(new AuthActions.AuthenticateFail(errorMessage))
+                        return handleError(errorRes)
                     })
                 );
             })
