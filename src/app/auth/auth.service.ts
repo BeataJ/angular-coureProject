96edecf5469c7102787086d22e4acafe6478ce7b
beataj <beata@emc2wave.com>
2021-11-23 18:50:41 -0800

    cleanup

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,19 +1,10 @@
 import { Injectable } from "@angular/core";
 import { Store } from "@ngrx/store";
 
-// import { User } from "./user.model";
 import * as fromApp from '../store/app.reducer';
 import * as AuthActions from './store/auth.actions';
 
 
-export interface AuthResponseData {
-    idToken: string,
-    email: string,
-    refreshToken: string,
-    expiresIn: string,
-    localId: string,
-    registered?: boolean
-}
 
 @Injectable({
     providedIn: 'root'
