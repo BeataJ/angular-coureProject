849495fc63914df24d6f5896ddaf741fa2a684f2
beataj <beata@emc2wave.com>
2021-12-10 16:56:58 -0800

    dispatch new RecipesActions.SetRecipes(recipes)

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from "@angular/core";
-import { HttpClient, HttpParams } from "@angular/common/http";
-import { map, tap, take, exhaustMap } from "rxjs/operators";
+import { HttpClient } from "@angular/common/http";
+import { map, tap } from "rxjs/operators";
+import { Store } from "@ngrx/store";
 
 import { RecipeService } from "../recipes/recipe.service";
 import { environment } from "../../../src/environment-app"
 import { Recipe } from "../recipes/recipe.model";
+import * as fromApp from '../store/app.reducer';
+import * as RecipesActions from '../recipes/store/recipe.action';
 
-import { AuthService } from "../auth/auth.service";
 
 @Injectable({
     providedIn: 'root'
@@ -15,7 +17,7 @@ export class DataStorageService {
     constructor(
         private http: HttpClient,
         private recipeService: RecipeService,
-        private authService: AuthService
+        private store: Store<fromApp.AppState>
         ) {}
 
     storeRecipes() {
@@ -40,7 +42,8 @@ export class DataStorageService {
                })
            }),
            tap(recipes => {
-               this.recipeService.setRecipes(recipes)
+            //    this.recipeService.setRecipes(recipes)
+            this.store.dispatch(new RecipesActions.SetRecipes(recipes))
            })
            
        )         
