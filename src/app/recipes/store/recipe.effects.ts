22778a31d7e4cddb904d1f22f2a87a71e05b2346
beataj <beata@emc2wave.com>
2021-12-11 22:44:41 -0800

    create RecipesEffects

---
+++ src/app/recipes/store/recipe.effects.ts
@@ -0,0 +1,33 @@
+import { Actions, Effect, ofType } from '@ngrx/effects';
+import { switchMap,map } from 'rxjs/operators';
+import { HttpClient } from "@angular/common/http";
+
+import * as RecipesActions from './recipe.action';
+import { Recipe } from '../recipe.model';
+import { environment } from '../../../environment-app';
+
+export class RecipeEffects {
+    @Effect()
+    fetchRecipes = this.actions$.pipe(
+        ofType(RecipesActions.FETCH_RECIPES),
+        switchMap(() => {
+            return this.http.get<Recipe[]>(environment.apiUrl)
+        }),
+        map(recipes => {
+            return recipes.map(recipe => {
+                return {
+                    ...recipe,
+                    ingredients: recipe.ingredients ? recipe.ingredients : []
+                }
+            })
+        }),
+        map(recipes => {
+            return new RecipesActions.SetRecipes(recipes);
+        })
+    )
+
+    constructor(
+        private actions$: Actions,
+        private http: HttpClient
+        ) {}
+}
