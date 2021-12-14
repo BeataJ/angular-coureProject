22674b1503aaa4cd86f4eb709f8b06dbdc62ff93
beataj <beata@emc2wave.com>
2021-12-14 00:30:50 -0800

    correct auth

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -6,26 +6,22 @@ import * as AuthActions from './store/auth.actions';
 
 
 
-@Injectable({
-    providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class AuthService {
-    // user = new BehaviorSubject<User>(null);
     private tokenExpirationTimer: any;
 
     constructor(
         private store: Store<fromApp.AppState>
-        ) {}
+    ) { }
 
-    
-    setLogoutTimer(exprationDuration: number) {
+    setLogoutTimer(expirationDuration: number) {
         this.tokenExpirationTimer = setTimeout(() => {
             this.store.dispatch(new AuthActions.Logout());
-        } , exprationDuration)
+        }, expirationDuration);
     }
 
     clearLogoutTimer() {
-        if(this.tokenExpirationTimer) {
+        if (this.tokenExpirationTimer) {
             clearTimeout(this.tokenExpirationTimer);
             this.tokenExpirationTimer = null;
         }
