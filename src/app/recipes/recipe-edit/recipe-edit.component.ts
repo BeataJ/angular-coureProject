d438e09cb353dd7135eb9fc944fd1ed3269e9f27
beataj <beata@emc2wave.com>
2021-12-14 19:10:23 -0800

    dispatch Add Recpie update recipe

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -6,7 +6,7 @@ import { map } from 'rxjs/operators';
 
 import { RecipeService } from '../recipe.service';
 import * as fromApp from '../../store/app.reducer';
-
+import * as RecipesActions from '../store/recipe.action';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -44,9 +44,14 @@ export class RecipeEditComponent implements OnInit {
     //     this.recipeForm.value['ingredients']
     //     )
     if(this.editMode) {
-      this.recipeService.updateRecipe(this.id, this.recipeForm.value)
+      // this.recipeService.updateRecipe(this.id, this.recipeForm.value)
+      this.store.dispatch(new RecipesActions.UpdateRecipe({
+        index: this.id,
+        newRecipe: this.recipeForm.value
+      }))
     } else {
-      this.recipeService.addRecipe(this.recipeForm.value)
+      // this.recipeService.addRecipe(this.recipeForm.value)
+      this.store.dispatch(new RecipesActions.AddRecipe(this.recipeForm.value));
     }
     this.onCancel()
   }
